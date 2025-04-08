3f02ec20073ff53666c1b3cc2eb6943603a6b873
Kengo Ishizuka <ishizuko@mail.uc.edu>
2025-04-08 13:41:07 -0400

    add some code in main.py

--- S10_Assginment10/mainPackage/main.py
+++ S10_Assginment10/mainPackage/main.py
@@ -9,3 +9,14 @@ if __name__ == "__main__":
 
     api = Api()
     parsed_json = api.get_api_data("https://disease.sh/v3/covid-19/states/Ohio")
+
+    #  Extract the data of total_case, total_death, case_today, death_today, and state from JSON
+    total_case = parsed_json["cases"]
+    total_death = parsed_json["deaths"]
+    case_today = parsed_json["todayCases"]
+    death_today = parsed_json["todayDeaths"]
+    state = parsed_json["state"]
+
+    # Print data
+    prt = PrintResult()
+    prt.print_result(total_case, total_death, case_today, death_today, state)
